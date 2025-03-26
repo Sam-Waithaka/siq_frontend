cf8f0aa0a5d63bbadf5455c714a0bece4e146bbb
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-03-26 20:35:15 +0300

    Add enviroment variables

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -5,7 +5,7 @@ const accessToken = localStorage.getItem("access_token");
 
 // Create an axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // Change this if your API URL is different
+  baseURL: process.env.REACT_APP_API_URL, // Change this if your API URL is different
   headers: {
     "Content-Type": "application/json",
     Authorization: accessToken ? `Bearer ${accessToken}` : "",
